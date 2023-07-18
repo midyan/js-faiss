37e46e32c9588f9801c2046d6fd759f3255dfb5a
Yan Nunes <yannunes@gmail.com>
2023-07-18 19:27:14 -0300

    feat: Basic TS Setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.eslint.json", "./packages/*/tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint", "import"],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: "packages/*/tsconfig.json",
+      },
+    },
+  },
+  rules: {
+    "eol-last": ["error", "always"],
+    /* IMPORT */
+    "import/no-unresolved": "error",
+  },
+};
+
+module.exports = config;
---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+const config = {
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  tabWidth: 2,
+  semi: true,
+  trailingComma: "all",
+};
+
+module.exports = config;
